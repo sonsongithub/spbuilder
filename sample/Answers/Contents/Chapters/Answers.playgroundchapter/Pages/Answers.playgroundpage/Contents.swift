487d2c8bc869ff9eec4878fd56e22243a3a430e0
sonson <yoshida.yuichi@gmail.com>
2016-06-24 02:04:46 +0900

    Removed samples and ruby.

--- sample/Answers/Contents/Chapters/Answers.playgroundchapter/Pages/Answers.playgroundpage/Contents.swift
+++
@@ -1,8 +0,0 @@
-//: Use ``show()`` to output text and ``ask()`` to input text.
-//#-code-completion(identifier, hide, AnswersLiveViewCommand)
-
-show("What is your name?")
-
-let name = ask("Name")
-
-show("Hi " + name)
